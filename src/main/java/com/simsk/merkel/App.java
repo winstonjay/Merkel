028656d7acdc3b021b1717028a59fe7ac4dc72e1
Karl Sims <karlsims.uk@gmail.com>
2019-08-20 17:34:01 +0100

    initial commit

---
+++ src/main/java/com/simsk/merkel/App.java
@@ -0,0 +1,9 @@
+package com.simsk.merkel;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/main/java/com/simsk/merkel/Node.java
@@ -0,0 +1,5 @@
+package com.simsk.merkel;
+
+public class Node {
+
+}
